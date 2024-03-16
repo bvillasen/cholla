1689020b44e8f9f9130c4929b56ef800c0e3d84d
bruno villasenor <bvillasen@gmail.com>
2024-03-16 13:26:18 -0700

    added inital rocshore implementation

--- rocshore/run_cholla_rocshore.sh
+++ rocshore/run_cholla_rocshore.sh
@@ -8,15 +8,16 @@ echo "Seting MI300A environment"
 source ${cholla_dir}/builds/setup.mi300.sh
 
 cholla_exec=$(find . -name 'cholla*')
-echo "Cholla executable: ${cholla_exec}"
+echo "Cholla executable: ${cholla_exec}"cat s
 
 
-run_cmd="mpirun -n 1 ${cholla_exec} parameter_file.txt > simulation_output.log"
+# run_cmd="mpirun -n 1 ${cholla_exec} parameter_file.txt > simulation_output.log"
+run_cmd="$OMPI_DIR/bin/mpirun -n 1 --mca osc ucx --mca pml ucx -x UCX_RNDV_THRESH=2048 ${cholla_exec} parameter_file.txt > simulation_output.log"
 echo "Run command: ${run_cmd}"
+$run_cmd
 
 # Parse FOM
-# grep "Time Total" simulation_output.log | tail -1  | tr ":" "ms" 
-average_timestep="$(grep "Time Total" simulation_output.log | tail -1 | awk -F: '{print $2}')"
-n_steps="$(grep "Average Times" simulation_output.log  | awk -F: '{print $2}')"
-echo "n steps: ${n_steps}"
-echo "average timestep: ${average_timestep}"
+# average_timestep="$(grep "Time Total" simulation_output.log | tail -1 | awk -F: '{print $2}')"
+# n_steps="$(grep "Average Times" simulation_output.log  | awk -F: '{print $2}')"
+# echo "n steps: ${n_steps}"
+# echo "average timestep: ${average_timestep}"
