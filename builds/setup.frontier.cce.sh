e1cf4648a5ad8e7331893d587ee0bac36fd3fe0d
bruno villasenor <bvillasen@gmail.com>
2026-07-09 14:07:21 -0700

    Update setup.frontier.cce.sh

--- builds/setup.frontier.cce.sh
+++ builds/setup.frontier.cce.sh
@@ -3,6 +3,8 @@
 #-- This script needs to be source-d in the terminal, e.g.
 #   source ./setup.frontier.cce.sh
 
+module load PrgEnv-cray/8.7.0
+module load cce/19.0.0
 module load rocm/7.2.0
 module load craype-accel-amd-gfx90a
 module load cray-hdf5 cray-fftw
